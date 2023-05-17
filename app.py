16e7ee89f656c0937c352a5d8165aeee71ffb958
pj8912 <jpintofun2b@gmail.com>
2023-05-17 14:30:13 +0530

    added exception and strip()

--- app.py
+++ app.py
@@ -7,9 +7,15 @@ os.environ['_BARD_API_KEY'] = os.getenv("BARD_API_KEY")
 
 bard = Bard()
 
-print('*'*50,'Terminal Chat with BARD','*'*50, '\n')
-while True:
-    user_input = input('\U0001F464 You: ')
-    print('')
-    print('\U0001F916 Bard:', bard.get_answer(user_input)['content'])
-    print('-'*90, "\n")
+print('\n','*'*50,'Terminal Chat with BARD','*'*50, '\n')
+try:
+    while True:
+        user_input = input('\U0001F464 You: ')
+        print('')
+        print('\U0001F916 Bard:', bard.get_answer(user_input.strip())['content'])
+        print('-'*100, "\n")
+except KeyboardInterrupt:
+    print('Ended Chat!')
+except:
+    print('Ended Chat!')
+
